19094caad3f77496fc5565351c8103880bd60367
Edward <v-tazho@hotmail.com>
2017-12-15 17:57:35 +0800

    add test project

---
+++ SharedCommunity.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+//Testing controller logic in ASP.NET Core
+//https://docs.microsoft.com/en-us/aspnet/core/mvc/controllers/testing
+namespace SharedCommunity.IntegrationTest
+{
+    
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
